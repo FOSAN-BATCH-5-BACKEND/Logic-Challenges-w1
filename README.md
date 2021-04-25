63047e06c1df746a104a339a919569eafa468de8
Adhityat Tristiawan <67779117+adhityat@users.noreply.github.com>
2021-04-25 12:25:09 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# logic-challenges-1
+
+## SOAL SUDAH ADA DI DALAM FILE - FILE YANG TELAH DISEDIAKAN BACA INSTRUKSINYA YAA
+
+## THANKYOU 
+
+## :rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket::rocket:
+
+##### 
---
+++ first-letter.js
@@ -0,0 +1,30 @@
+/**
+---------------------
+First Letter Grouping
+---------------------
+
+Diberikan sebuah variabel `name`. Buatlah sebuah script untuk menampilkan nama tersebut
+berada di group ke-berapa, dengan aturan sebagai berikut:
+- Jika huruf pertama dari `name` adalah a, b, c, d maka tampilkan 'Masuk group pertama'
+- Jika huruf pertama dari `name` adalah e, f, g, h maka tampilkan 'Masuk group kedua'
+- Selain itu tampilkan 'Masuk group terakhir'
+
+Contoh:
+- `name` = 'alwan', output: 'Masuk group pertama'
+- `name` = 'fitra', output: 'Masuk group kedua'
+- `name` = 'kipli', output: 'Masuk group terakhir'
+- `name` = 'songkro', output: 'Masuk group terakhir'
+
+*/
+
+function firstLetter(name){
+
+}
+
+console.log(firstLetter('alwan')) //Masuk group pertama
+console.log(firstLetter('bhagas'))// Masuk group pertama
+console.log(firstLetter('fitra')) //Masuk group kedua
+console.log(firstLetter('hary')) //Masuk group kedua
+console.log(firstLetter('yupek')) // Masuk group terakhir
+console.log(firstLetter('kipli')) // Masuk group terakhir
+console.log(firstLetter('songkro')) //Masuk group terakhir
