eac4c3b199dbc100cb25551957ae358e514f216e
syafiq <syafiqardiansyah30@gmail.com>
2021-04-26 12:04:09 +0700

    first-letter

--- bandingkan-angka.js
+++ bandingkan-angka.js
@@ -6,12 +6,18 @@ Jika kedua angka bernilai sama, function akan me-return -1.
 */
 
 function bandingkanAngka(angka1, angka2) {
-  // you can only write your code here!
+    if(angka2 > angka1){
+        return "true";
+    }else if(angka1 == angka2){
+        return "-1";
+    }else{
+        return "false";
+    }
 }
 
 // TEST CASES
-console.log(bandingkanAngka(5, 8)); // true
-console.log(bandingkanAngka(5, 3)); // false
-console.log(bandingkanAngka(4, 4)); // -1
-console.log(bandingkanAngka(3, 3)); // -1
-console.log(bandingkanAngka(17, 2)); // false
+bandingkanAngka(5, 8); // true
+bandingkanAngka(5, 3); // false
+bandingkanAngka(4, 4); // -1
+bandingkanAngka(3, 3); // -1
+bandingkanAngka(17, 2); // false
